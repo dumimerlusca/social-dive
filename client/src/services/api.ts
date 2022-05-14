b8be528b58abd285910d5bcb0d987aee6931d988
Dumitru <dumitrumerlusca@gmail.com>
2022-05-14 22:20:02 +0300

    Some changes for deploy

--- client/src/services/api.ts
+++ client/src/services/api.ts
@@ -1,38 +1,37 @@
-import axios from "axios";
-export const SERVER_URL = "http://localhost:5000";
+import axios from 'axios';
+export const SERVER_URL =
+  process.env.NODE_ENV === 'production' ? 'https://social-media-app-backed.vercel.app' : 'http://localhost:5000';
 export const BASE_API_URL = `${SERVER_URL}/api`;
-export const userImageUrl = (userId: string | undefined) =>
-	BASE_API_URL + `/users/${userId}/photo`;
-export const postImageUrl = (postId: string | undefined) =>
-	BASE_API_URL + `/posts/${postId}/photo`;
+export const userImageUrl = (userId: string | undefined) => BASE_API_URL + `/users/${userId}/photo`;
+export const postImageUrl = (postId: string | undefined) => BASE_API_URL + `/posts/${postId}/photo`;
 
 const getConfig = (token: string | null) => ({
-	headers: {
-		authorization: `Bearer ${token}`,
-	},
+  headers: {
+    authorization: `Bearer ${token}`,
+  },
 });
 
 export const get = async (url: string) => {
-	const token = getToken();
-	return axios.get(BASE_API_URL + url, getConfig(token));
+  const token = getToken();
+  return axios.get(BASE_API_URL + url, getConfig(token));
 };
 export const post = async (url: string, data?: any) => {
-	const token = getToken();
-	return axios.post(BASE_API_URL + url, data ?? {}, getConfig(token));
+  const token = getToken();
+  return axios.post(BASE_API_URL + url, data ?? {}, getConfig(token));
 };
 export const put = async (url: string, data: any) => {
-	const token = getToken();
-	return axios.put(BASE_API_URL + url, data, getConfig(token));
+  const token = getToken();
+  return axios.put(BASE_API_URL + url, data, getConfig(token));
 };
 export const patch = async (url: string, data?: any) => {
-	const token = getToken();
-	return axios.patch(BASE_API_URL + url, data ?? {}, getConfig(token));
+  const token = getToken();
+  return axios.patch(BASE_API_URL + url, data ?? {}, getConfig(token));
 };
 export const APIdelete = async (url: string) => {
-	const token = getToken();
-	return axios.delete(BASE_API_URL + url, getConfig(token));
+  const token = getToken();
+  return axios.delete(BASE_API_URL + url, getConfig(token));
 };
 
 export function getToken() {
-	return localStorage.getItem("token");
+  return localStorage.getItem('token');
 }
